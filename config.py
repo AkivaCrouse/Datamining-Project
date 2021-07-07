d6a4e954a995d28f2ea16374e48ef0ac20f0bd7b
Aaron Reuven <roni.reuven@gmail.com>
2021-07-07 22:23:52 +0300

    Table constants and requirements.txt update

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
-
-
 # SQL defaults
 HOST = 'localhost'
 USER = 'root'
 DATABASE = 'Coindesk'
+# Table names
+ARTICLES_TABLE = 'Articles'
+AUTHORS_TABLE = 'Authors'
+SUMMARIES_TABLE = 'Summaries'
+TAGS_TABLE = 'Tags'
+CATEGORIES_TABLE = 'Categories'
+TAGS_ARTICLES_TABLE = 'Tags_in_articles'
+AUTHORS_ARTICLES_TABLE = 'Authors_in_articles'
+
