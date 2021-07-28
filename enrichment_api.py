ead778f589877c06cb1c05f63f4a4ca5aeb37ce0
Aaron Reuven <roni.reuven@gmail.com>
2021-07-28 15:34:11 +0300

    Changed from pages to pagesize because we can only extract 100 at a time with the free usage of the api

--- enrichment_api.py
+++ enrichment_api.py
@@ -6,7 +6,6 @@ from Coindesk_Scraper import Article
 
 
 def enrich_tag(tag, num_article, from_date=None, to_date=None, domains=None, sort_by='publishedAt'):
-    pages = (num_article // 100) + (num_article % 100 > 0)
     from_date_str = ''
     to_date_str = ''
     domains_str = ''
@@ -17,17 +16,16 @@ def enrich_tag(tag, num_article, from_date=None, to_date=None, domains=None, sor
         to_date_str = f'&to={to_date}'
     if domains is not None:
         domains_str = f'&domains={",".join(domains)}'
-
-    url = f'https://newsapi.org/v2/everything?q={tag}&language=en&excludeDomains=coindesk.com&apiKey={API_KEY}' \
+    url = f'https://newsapi.org/v2/everything?q={tag}&pageSize={num_article}&language=en&excludeDomains=coindesk.com&apiKey={API_KEY}' \
           f'{sort_by_str}{from_date_str}{to_date_str}{domains_str}'
-    responses = grequests.map((grequests.get(url+f'&page={i}') for i in range(1, pages + 1)))
-    responses_json = [response.json() for response in responses]
+    response = requests.get(url)
+    json = response.json()
     articles = []
-    for json in responses_json:
-        for article in json['articles']:
-            if article['author'] is None:
-                article['author'] = 'Unknown'
-            articles.append(Article(article['title'], article['description'], [article['author']], article['url'],
-                                    [tag], datetime.strptime(article['publishedAt'], API_DATE_FORMAT), ['Enriched data']))
+    for article in json['articles']:
+        if article['author'] is None:
+            article['author'] = 'Unknown'
+        if article['description'] is None:
+            article['description'] = ''
+        articles.append(Article(article['title'], article['description'], [article['author']], article['url'],
+                                [tag], datetime.strptime(article['publishedAt'], API_DATE_FORMAT), ['Enriched data']))
     return articles
-
