60a7b71401b9b90acdda7c80676abe62cb38c05c
Aaron Reuven <roni.reuven@gmail.com>
2021-07-28 15:20:20 +0300

    Unknown Author error handled

--- enrichment_api.py
+++ enrichment_api.py
@@ -1,12 +1,12 @@
 import grequests
 import requests
-import json
 from datetime import datetime
 from config import *
 from Coindesk_Scraper import Article
 
 
 def enrich_tag(tag, num_article, from_date=None, to_date=None, domains=None, sort_by='publishedAt'):
+    pages = (num_article // 100) + (num_article % 100 > 0)
     from_date_str = ''
     to_date_str = ''
     domains_str = ''
@@ -20,11 +20,14 @@ def enrich_tag(tag, num_article, from_date=None, to_date=None, domains=None, sor
 
     url = f'https://newsapi.org/v2/everything?q={tag}&language=en&excludeDomains=coindesk.com&apiKey={API_KEY}' \
           f'{sort_by_str}{from_date_str}{to_date_str}{domains_str}'
-    response = requests.get(url)
-    response_json = response.json()
+    responses = grequests.map((grequests.get(url+f'&page={i}') for i in range(1, pages + 1)))
+    responses_json = [response.json() for response in responses]
     articles = []
-    for article in response_json['articles']:
-        articles.append(Article(article['title'], article['description'], article['author'], article['url'],
-                                tag, datetime.strptime(article['publishedAt'], API_DATE_FORMAT), 'Enriched data'))
-
+    for json in responses_json:
+        for article in json['articles']:
+            if article['author'] is None:
+                article['author'] = 'Unknown'
+            articles.append(Article(article['title'], article['description'], [article['author']], article['url'],
+                                    [tag], datetime.strptime(article['publishedAt'], API_DATE_FORMAT), ['Enriched data']))
     return articles
+
